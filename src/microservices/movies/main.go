29c5515fc16640976add4f09789c8a60b1d30206
Mkuzya <k.a.marina.g@gmail.com>
2025-08-14 23:33:49 +0300

    fix(db): add startup retry loop for monolith and movies to wait for Postgres

--- src/microservices/movies/main.go
+++ src/microservices/movies/main.go
@@ -53,10 +53,18 @@ func initDB() {
 		log.Fatal(err)
 	}
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
+        // Retry until DB becomes available
+        maxWait := 30 * time.Second
+        start := time.Now()
+        for {
+                if err = db.Ping(); err == nil {
+                        break
+                }
+                if time.Since(start) > maxWait {
+                        log.Fatal(err)
+                }
+                time.Sleep(1 * time.Second)
+        }
 	log.Println("Successfully connected to database")
 }
 
